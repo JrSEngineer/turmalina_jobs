6b89ba9179377298d1bd73ed4bb778f9b5914429
JrSEngineer <jr.s.engineer@gmail.com>
2024-12-25 16:54:42 -0300

    Creting light and dark themes.

---
+++ lib/src/shared/themes/light_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(
+  colorScheme: ColorScheme(
+    brightness: Brightness.light,
+    primary: const Color(0xFF321834),
+    onPrimary: const Color(0xFFFAF4FF),
+    secondary: const Color(0xFF3A333A),
+    onSecondary: const Color(0xFF321834),
+    error: Colors.red.shade900,
+    onError: const Color(0xFFFAF4FF),
+    background: const Color(0xFFFAF4FF),
+    onBackground: const Color(0xFF321834),
+    surface: Colors.blue,
+    onSurface: const Color(0xFF321834),
+  ),
+);
