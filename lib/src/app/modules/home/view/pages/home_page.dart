b3aa721598b5007e25065b8efb33e4ca045efbd5
JrSEngineer <jr.s.engineer@gmail.com>
2024-12-27 00:19:42 -0300

    Adding background image

--- lib/src/app/modules/home/view/pages/home_page.dart
+++ lib/src/app/modules/home/view/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:turmalina_jobs/src/shared/widgets/app_container.dart';
+import 'package:turmalina_jobs/src/shared/widgets/general_header_widget/general_header_image_widget.dart';
+import 'package:turmalina_jobs/src/shared/widgets/general_header_widget/general_header_text.dart';
+import 'package:turmalina_jobs/src/shared/widgets/general_header_widget/general_header_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,8 +14,35 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const AppContainer(
-      child: Center(child: Text('HOME PAGE')),
+    final componentImageSize = MediaQuery.sizeOf(context).height * 0.04;
+    const componentImageRadius = 45.0;
+    const componentImageRightMargin = 12.0;
+    const internalPadding = 12.0;
+
+    const image = 'https://images.pexels.com/photos/93820/pexels-photo-93820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+    return AppContainer(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          GeneralHeaderWidget(
+            internalPadding: internalPadding,
+            image: GeneralHeaderImageWIdget(
+              componentImageSize: componentImageSize,
+              componentImageRightMargin: componentImageRightMargin,
+              componentImageRadius: componentImageRadius,
+              imageUrl: image,
+            ),
+            title: GeneralHeaderText(
+              content: 'Olá, Usuário',
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+            subTitle: GeneralHeaderText(
+              content: 'Boas Vindas!',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
