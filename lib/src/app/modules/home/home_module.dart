ac0ed611f76591fc34a7ee67cf55bce6ce17051c
JrSEngineer <jr.s.engineer@gmail.com>
2024-12-27 23:11:33 -0300

    Removing page in diferent context.

--- lib/src/app/modules/home/home_module.dart
+++ lib/src/app/modules/home/home_module.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:turmalina_jobs/src/app/modules/home/view/pages/chat_page.dart';
 import 'package:turmalina_jobs/src/app/modules/home/view/pages/home_page.dart';
 import 'package:turmalina_jobs/src/app/modules/home/view/pages/profile_page.dart';
 
@@ -7,7 +6,6 @@ class HomeModule extends Module {
   @override
   void routes(RouteManager r) {
     r.child('/', child: (_) => const HomePage());
-    r.child('/chat', child: (_) => const ChatPage());
     r.child('/profile', child: (_) => const ProfilePage());
   }
 }
